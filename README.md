9b83082b2276701a4d503ab03ea39329f5ecb8c1
Nathanael Cruz <nathanael.cruz.fd@gmail.com>
2020-08-04 23:20:54 -0300

    Finished working server

---
+++ README.md
@@ -0,0 +1,12 @@
+# Funcionalidades
+
+## Conexões
+
+ - Rota para listar o total de conexões
+ - Rota para criar conexão
+
+## Aulas
+
+ - Rota para criar aulas
+ - Rota para listar aulas
+   - Filtar por matéria, dia da semana e horário
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,12 @@
-import express from 'express'
+import express from 'express';
+import cors from 'cors';
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-    const users = [
-        {nome: "Nathanael", age: 26},
-        {nome: "Samara", age: 20}
-    ]
-    return response.json(users)
-})
+app.use(cors())
+app.use(express.json())
+app.use(routes)
 
 app.listen(3330);
 
